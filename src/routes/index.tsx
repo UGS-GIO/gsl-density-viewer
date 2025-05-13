d6188fdc61a7ec2ea4a593479e233cf16b4289c8
Clinton Lunn <clunn@utah.gov>
2025-05-13 17:17:57 -0600

    feat: add shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -9,6 +10,9 @@ function Home() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button variant="default" className="mt-4">
+        Press Me
+      </Button>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path, { resolve } from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
@@ -8,4 +9,9 @@ export default defineConfig({
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
