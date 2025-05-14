a2404f7373529458bf78950bf32988c00fd0dde5
Clinton Lunn <clunn@utah.gov>
2025-05-14 12:58:05 -0600

    refactor: add types, update configs for tailwind

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import path, { resolve } from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
